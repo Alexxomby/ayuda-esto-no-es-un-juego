545e398cef4b1529aaafdd0cffd1628eee62e184
Alexxomby <72172506+Alexxomby@users.noreply.github.com>
2023-03-16 14:42:44 -0600

    unlittle ajuste a las preguntas

--- Preguntados/data/data.js
+++ Preguntados/data/data.js
@@ -3,32 +3,32 @@
 
     {
         question:"¿Cuál es el planeta de origen de los Transformers?",
-        choices:["A) Tatooine", "B) Cybertron", "C) Krypton ", "D) endor"],
-        answer: "B) Cybertron",
+        choices:["Tatooine", "Cybertron", "Krypton ", "endor"],
+        answer: "Cybertron",
     },
 
     {
         question:"¿Cuál es el apellido del famoso mago Harry?",
-        choices:["A) Weasley ", "B) Malfoy", "C) Granger " , "D) Potter"],
-        answer: "D) Potter",
+        choices:["Weasley ", "Malfoy", "Granger " , "Potter"],
+        answer: "Potter",
     },  
 
     {
         question:"¿Cuál es la marca de automóviles que produce el modelo Mustang?",
-        choices:["A) Dodge", "B) Chevrolet", "C) Ford" , "D) Toyota"],
-        answer: "C) Ford",
+        choices:["Dodge", "Chevrolet", "Ford" , "Toyota"],
+        answer: "Ford",
     },
 
     {
         question:"¿En qué ciudad se encuentra la Torre Eiffel?",
-        choices:["A) Nueva York", "B) París", "C) Londres", "D) Berlín"],
-        answer: "B) París",
+        choices:["Nueva York", "París", "Londres", "Berlín"],
+        answer: "París",
     },
     
     {
         question:"¿Cuál es el nombre de la nave espacial de Han Solo en Star Wars?",
-        choices:["A) Discovery", "B) Enterprise", "C) Halcon Milenario", "D) USG Ishimura"],
-        answer: "C) Millennium Falcon",
+        choices:["Discovery", "Enterprise", "Halcon Milenario", "USG Ishimura"],
+        answer: "Halcon Milenario",
     },
   
 
