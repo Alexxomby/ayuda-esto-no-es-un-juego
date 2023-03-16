2b2fb9dd5735564c0518a84ef07e8c3957358eec
Alexxomby <72172506+Alexxomby@users.noreply.github.com>
2023-03-16 13:06:26 -0600

    Ayuda no se muestra el score ni la correct answer

--- Preguntados/app.js
+++ Preguntados/app.js
@@ -3,15 +3,40 @@ import { questions } from "./data/questions.js";
 import { Quiz } from "./modelo/Quiz.js";
 import { UI } from "./modelo/UI.js";
 
+
+/**
+ * 
+ * @param {Quiz} quiz the main quiz object
+ * @param {UI} ui ui object
+ */
+const renderPage =  (quiz,ui) =>{
+
+
+    if(quiz.idEnded()){
+
+        console.log(quiz.scorepos)
+        ui.showScores(quiz.scorepos);
+    }else{
+
+    ui.showQuestion(quiz.getQuestionIndex().text);
+    ui.showChoices(quiz.getQuestionIndex().choices, (currentChoice) =>{
+
+        quiz.guess(currentChoice)
+        renderPage(quiz,ui)
+        });
+
+        ui.showProgress(quiz.questionIndex + 1 , quiz.questions.length);
+    }
+};
+
+
 function main() {
 
     const quiz = new Quiz(questions);
     const ui = new UI();
+
+    renderPage(quiz,ui)
     
-    ui.showQuestion(quiz.getQuestionIndex().text);
-
-    ui.showChoices(quiz.getQuestionIndex().choices);
-
 }
 
 main();
--- Preguntados/modelo/UI.js
+++ Preguntados/modelo/UI.js
@@ -18,21 +18,48 @@ export class UI{
      * 
      * @param {string[]} choices opciones de las preguntas
      */
-    showChoices(choices){
+    showChoices(choices, callback){
         
     const choicesContainer = document.getElementById("choices");
+    choicesContainer.innerHTML = '';
 
         for (let i = 0; i < choices.length; i++){
 
             const buton = document.createElement("button");
-            buton.innerText = "algo";
+            buton.innerText = choices[i];
             buton.className = 'button';
+            buton.addEventListener('click', () => callback(choices[i]));
 
             choicesContainer.append(buton);
         }
+    }
 
+    /**
+     * 
+     * @param {numero} scorepos 
+     */
+    showScores(scorepos){
 
+        const quizEndHTML = `
+            <h1> Resultado </h1>
+            <h2>Tu puntuacion: ${scorepos}</h2>
+            `;
 
+        const element = getElementById("quiz");
+
+        element.innerHTML = quizEndHTML;
+    }
+
+    /**
+     * 
+     * @param {numero} currentIndex the courrent index of the quiz
+     * @param { numero} total the total questions
+     */
+    showProgress(currentIndex, total){
+
+        const element = document.getElementById("progress");
+        element.innerHTML = `Question ${currentIndex} of ${total}`;
+    
     }
 
 }
